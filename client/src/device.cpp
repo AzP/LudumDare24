1dfe7c5ae6f0ad0d832a92fb63e91189b1749d6e
Peter Asplund <peter.azp@gmail.com>
2012-08-25 18:16:53 +0200

    Refactor to split out device generation and setup.

---
+++ client/src/device.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cassert>
+
+#include "device.h"
+
+float CDevice::getFPS()
+{
+	return m_driver->getFPS();
+}
+
+float CDevice::getFrameTime()
+{ 
+	int frametime = (m_timer->getTime() - m_lasttime)/1000.0;
+	m_lasttime = m_timer->getTime();
+	return frametime;
+}
+
+
+CDevice::CDevice(MyEventReceiver& receiver)
+: m_lasttime(0)
+{
+	setupIrrlicht(receiver);
+}
+
+void CDevice::setupIrrlicht(MyEventReceiver& receiver)
+{
+	// Create device
+	const dimension2d<u32> windowDimensions(windowX, windowY);
+	m_device = createDevice(video::EDT_OPENGL, windowDimensions, 
+		32, false, true, true, &receiver); 
+	m_device->setResizable(false); 
+
+	// Check if device could be created. If not, terminate.
+	if (m_device == 0) 
+	{
+		cerr << "Could not create device" << endl;
+	}
+
+	m_driver = m_device->getVideoDriver();
+	m_sceneMgr = m_device->getSceneManager();
+
+	m_env = m_device->getGUIEnvironment();
+	m_font = m_env->getFont("../media/fonthaettenschweiler.bmp");
+	assert(m_font);
+
+	/*************
+	  Create the camera
+	 *************/
+	m_cameraNode = m_sceneMgr->addCameraSceneNodeFPS();
+	m_cameraNode->setFOV(PI/2.0f);
+	//m_cameraNode->setPosition(core::vector3df(0.0f,0.0f,-15.0f));
+	//m_cameraLookAtNode = m_sceneMgr->addEmptySceneNode(); //node the camera is set to look at
+
+	/************
+	 * Set up fog
+	 ************/
+	m_driver->setFog(video::SColor(30,125,125,138), video::EFT_FOG_EXP, 50.f, 100.f, .01f, true, true);
+
+	/*******************
+	  Creates the other players
+	 *******************/
+
+	/************
+	  Add the gui
+	 ************/
+	m_driver->getMaterial2D().TextureLayer[0].BilinearFilter=true;
+	m_driver->getMaterial2D().AntiAliasing=video::EAAM_FULL_BASIC;
+
+	//Create a timer to keep track of time
+	m_timer = m_device->getTimer(); 
+}
+
+
